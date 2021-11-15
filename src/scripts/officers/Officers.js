b50fba2fa9df66cdca61192722d709f7e0e5f956
helloamandaball <helloamandaball@gmail.com>
2021-11-15 09:00:44 -0500

    updates

--- src/scripts/officers/Officers.js
+++ src/scripts/officers/Officers.js
@@ -1,7 +1,7 @@
 export const Officers = (officersObject) => {
     return `
         <div class="officers-list-card">
-            <div class="officerImg"><img src="https://www.digidescorp.com/wp-content/uploads/2016/08/FPO-Image-Squares.jpg" alt="image"></div>
+            <div class="officerImg"><img src="https://thumbs.dreamstime.com/b/special-police-badge-silver-police-badge-isolated-white-background-101516104.jpg" alt="image"></div>
             <div class="officerName">${officersObject.name}</div>
         </div>
     `
